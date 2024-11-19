d4bffedb5c1c3e63b9240fc05637e4411e6ee8e0
Mekhi Woods <mekhidw@hawaii.edu>
2024-11-18 14:23:08 -1000

    Updated operation guide

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ from astropy.visualization import ZScaleInterval
 from astropy.stats import sigma_clipped_stats
 from astropy.io import fits
 from astropy.wcs import WCS
+from photutils.isophote import EllipseGeometry, EllipseSample, Isophote
+from photutils.aperture import EllipticalAperture, CircularAperture, EllipticalAnnulus, CircularAnnulus
 
 class PetrosianObject:
     """
@@ -46,63 +48,103 @@ class PetrosianObject:
         return
     def __str__(self):
         return f"[{self.ID}] {self.realCoords} | {self.pixCoords} | {self.z}"
-    def circular_fit(self, data, err, nRings: int = 30, rLim: int = 50):
-        self.radius, self.SB, self.SB_err = np.array([]), np.array([]), np.array([])
-        radius = np.arange(0.1, rLim, rLim / nRings)
-        for r in radius:
-            aper = CircularAperture([self.pixCoords[0], self.pixCoords[1]], r)
-            sums, sums_err = aper.do_photometry(data=data, error=err)
-            if sums[0] > 0:
-                self.radius = np.append(self.radius, r)
-                self.SB = np.append(self.SB, sums[0]/ (4*np.pi*(r**2)))
-                self.SB_err = np.append(self.SB_err, np.abs(sums_err[0] / (4*np.pi*(r**2))))
-        # self.SB_err[0] = 0.00  # Some unknown error is giving extreme value
-        return
-    def get_petrosian_radius(self, adda: float = 0.2, sens: float = 0.1):
-        for i in range(2, len(self.radius)):
-            local_area = (self.radius[i]**2)*np.pi
-            local_intensity = self.SB[i]
-            integrated_intensity = adda * simpson(y=self.SB[:i], x=self.radius[:i])
+    def SSDS_fit(self, data, err, r_step: int = 1, r_limit: int = 30, r_step_limit: int = 40):
+        """
+        Fits the SB profile using the SSDS method
+        """
+        self.radius, self.SB, self.SB_err, self.annulus_SB, self.annulus_SB_err, self.fancy_R = (
+            np.array([]), np.array([]), np.array([]), np.array([]), np.array([]), np.array([]))
 
-            if np.abs(local_intensity-integrated_intensity) < sens:
-                self.petroR = self.radius[i]
-                self.petroR_kpc = pix_to_kpc(self.z, self.petroR)
-                print(f'[+++] {self.ID} | Petrosian Radius: {self.petroR_kpc}')
-                return
-        if np.isnan(self.petroR):
-            print('[!!!] No Petrosian Radius Found!')
-            return
-    def plot(self, data):
-        fig, ax = plt.subplots(1, 2,
-                               figsize=(12, 4),
-                               gridspec_kw={'width_ratios': [1, 2]},
-                               constrained_layout=True)
+        # Steps from center to radius limit
+        r_range = np.arange(0.1, r_limit, r_limit / r_step_limit)
+        for r in r_range:
+            # Create geometries
+            annulus = CircularAnnulus([self.pixCoords[0], self.pixCoords[1]], 0.8*r, 1.25*r)
+            elipse = EllipticalAperture([self.pixCoords[0], self.pixCoords[1]], r, r)
+
+            # Get photmetric data
+            annulus_intensity, annulus_intensity_err = annulus.do_photometry(data=data, error=err)
+            elipse_intensity, elipse_intensity_err = elipse.do_photometry(data=data, error=err)
+
+            # Get instantaneous area
+            annulus_area = np.pi*(((1.25*r)**2) - ((0.8*r)**2))
+            elipse_area = np.pi*(r**2)
+
+            # Store data
+            self.radius = np.append(self.radius, r)
+            self.SB = np.append(self.SB, elipse_intensity[0] / elipse_area)
+            self.SB_err = np.append(self.SB_err, elipse_intensity_err[0])
+            self.annulus_SB = np.append(self.annulus_SB, annulus_intensity[0] / annulus_area)
+            self.annulus_SB_err = np.append(self.annulus_SB_err, annulus_intensity_err[0])
+            self.fancy_R = np.append(self.fancy_R,
+                                     float(get_constants()['ADDA'])*((elipse_intensity[0] / elipse_area) / (annulus_intensity[0] / annulus_area)))
+
+        # Calculate petrosian radius
+        radius_copy = np.copy(self.radius)
+        difference_array = np.abs(self.SB - self.fancy_R)
+        radius_copy = radius_copy[difference_array < float(get_constants()['PETRO_SENS'])]
+        difference_array = difference_array[difference_array < float(get_constants()['PETRO_SENS'])]
+        if len(difference_array) != 0:
+            self.petroR = radius_copy[difference_array == np.min(difference_array)][0]
+            self.petroR_kpc = pix_to_kpc(self.z, self.petroR)
+        else:
+            self.petroR, self.petroR_kpc = np.nan, np.nan
+
+        return
+    def plot(self, data, save_fig: bool = False):
+        # Set up subplot stuff
+        fig = plt.figure(figsize=(11, 5), constrained_layout=True)
+        gs = plt.GridSpec(nrows=6, ncols=2)
+        ax1 = fig.add_subplot(gs[:, 0])
+        ax21 = fig.add_subplot(gs[:3, 1])
+        ax22 = fig.add_subplot(gs[3:, 1])
 
         # Replot image data
         aper = CircularAperture([self.pixCoords[0], self.pixCoords[1]], self.petroR)
-        ax[0].imshow(data,
+        ax1.imshow(data,
                      vmin=np.median(data[~np.isnan(data)])-0.5,
                      vmax=np.median(data[~np.isnan(data)])+0.5)
-        ax[0].plot(self.pixCoords[0], self.pixCoords[1], marker='+', color='k',
+        ax1.plot(self.pixCoords[0], self.pixCoords[1], marker='+', color='k',
                    label=(f'ID: {int(self.ID)}'+' | $z_{?}$'+f': {self.z}\n'+
                           f'RA: {round(self.realCoords[0], 4)}, DEC: {round(self.realCoords[1], 4)}'))
-        aper.plot(ax=ax[0], color='red', label='Petrosian Radius')
-        ax[0].set_xlabel('[pix]'); ax[0].set_ylabel('[pix]')
-        ax[0].set_ylim(self.pixCoords[1] - np.max(self.radius),
+        aper.plot(ax=ax1, color='red', label='Petrosian Radius')
+        ax1.set_xlabel('[pix]'); ax1.set_ylabel('[pix]')
+        ax1.set_ylim(self.pixCoords[1] - np.max(self.radius),
                        self.pixCoords[1] + np.max(self.radius))
-        ax[0].set_xlim(self.pixCoords[0] - np.max(self.radius),
+        ax1.set_xlim(self.pixCoords[0] - np.max(self.radius),
                        self.pixCoords[0] + np.max(self.radius))
-        ax[0].legend(loc='best')
+        ax1.legend(loc='upper left')
 
         # Plot SB v. Radius
-        ax[1].errorbar(pix_to_kpc(self.z, self.radius), self.SB, yerr=self.SB_err, fmt='o', color='blue')
-        ax[1].axvline(self.petroR_kpc, color='red', label=f'Petrosian Radius: {round(self.petroR_kpc, 2)}kpc')
-        ax[1].legend(loc='best')
-        ax[1].set_xlabel('Radius [kpc]')
-        ax[1].set_ylabel('Surface Brightness [counts/pix$^{2}$]')
+        ax21.errorbar(pix_to_kpc(self.z, self.radius), self.SB, yerr=self.SB_err, fmt='o', color='blue')
+        ax21.axvline(self.petroR_kpc, color='red', label=f'Petrosian Radius: {round(self.petroR_kpc, 2)}kpc')
+        ax21.legend(loc='best')
+        ax21.set_xlabel('Radius [kpc]')
+        ax21.set_ylabel('[counts/pix]')
+
+        # Plot residuaul data
+        ax22.scatter(pix_to_kpc(self.z, self.radius), self.SB, marker='o', color='g', label='SB')
+        ax22.scatter(pix_to_kpc(self.z, self.radius), self.fancy_R, marker='+', color='k', label='Fancy R')
+        ax22.scatter(pix_to_kpc(self.z, self.radius), np.abs(self.fancy_R - self.SB), marker='.', color='orange', label='Resid')
+        ax22.set_xlabel('Radius [kpc]')
+        ax22.set_ylabel('Surface Brightness')
+        ax22.legend()
+
+        if save_fig:
+            plt.savefig(f'{self.ID}.png')
 
         plt.show()
+
         return
+    def SB_plot(self):
+        fig, axs = plt.subplots(2, 1)
+        axs[0].errorbar(self.radius, self.SB, yerr=self.SB_err, color='r', fmt='o')
+        axs[0].axvline(self.petroR)
+        axs[1].scatter(self.radius, self.SB, marker='o', color='g', label='SB')
+        axs[1].scatter(self.radius, self.fancy_R, marker='+', color='k', label='Fancy R')
+        axs[1].scatter(self.radius, np.abs(self.fancy_R - self.SB), marker='.', color='orange', label='Resid')
+        axs[1].legend()
+        plt.show()
 
 # General Functions ================================================================================================= #
 def get_constants(constant_loc='constants.txt'):
@@ -179,7 +221,8 @@ def pix_to_kpc(z: float, d: float):
                                    d * float(CONSTANTS['PIX_SCALE']) * (np.pi/(180*3600)) * 1000)
 
 # Main Functions ==================================================================================================== #
-def run(fits_path: str, target_path: str = '', fit_type: str = 'std') -> None:
+def run(fits_path: str, target_path: str = '', fit_type: str = 'std',
+        r_step: int = 1, r_limit: int = 30, r_step_limit: int = 40) -> None:
     """
     Main process.
     :param fits_path: path to fits file
@@ -207,24 +250,19 @@ def run(fits_path: str, target_path: str = '', fit_type: str = 'std') -> None:
                                              pixCoords=target_data['pixCoords'][i], z=target_data['z'][i]))
 
     # Fitting SB profiles
-    print("[+++] Fitting data with circular fit...")
+    print("[+++] Fitting data with SSDS fit...")
     if fit_type == 'std':
         for obj in allPetrosians:
-            obj.circular_fit(data, err)
-
-    # Calculating petrosian radius
-    print("[+++] Finding petrosian radii...")
-    for obj in allPetrosians:
-        obj.get_petrosian_radius(0.2, 0.1)
+            obj.SSDS_fit(data, err, r_step, r_limit, r_step_limit)
 
     # Display results
     print('[+++] Displaying data...')
     for obj in allPetrosians:
-        obj.plot(data)
+        obj.plot(data, True)
 
     # Save data
     print('[+++] Saved data to... results.txt')
-    with open('results.txt', 'w') as f:
+    with open('saved/results.txt', 'w') as f:
         f.write('# M.D. Woods -- using sens=0.1\n')
         f.write('ID,RA,DEC,Z,PETRO_PIX,PETRO_KPC\n')
         for obj in allPetrosians:
@@ -238,4 +276,5 @@ if __name__ == '__main__':
     mainPath = r'fits/jw02514125001_03201_00002_nrca2_i2d.fits' # Some on image
     targetPath = r'files/targets.csv'
 
-    run(mainPath, targetPath, 'std')
+    fitting_params = {'r_step': 0.1, 'r_limit': 30, 'r_step_limit': 30}
+    run(mainPath, targetPath, 'std', **fitting_params)
