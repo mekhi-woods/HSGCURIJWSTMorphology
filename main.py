2de5e6b4de75acc25ad9fbce3205f3c33ebba300
Mekhi Woods <mekhidw@hawaii.edu>
2024-11-20 10:08:38 -1000

    updates

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ import time as systime
 import numpy as np
 import matplotlib.pyplot as plt
 from astropy.coordinates import SkyCoord
-from photutils.aperture import CircularAperture
-from scipy.integrate import simpson
 from astropy.visualization import ZScaleInterval
 from astropy.stats import sigma_clipped_stats
 from astropy.io import fits
@@ -271,10 +269,13 @@ def run(fits_path: str, target_path: str = '', fit_type: str = 'std',
     return
 
 if __name__ == '__main__':
+    start = systime.time()  # Runtime tracker
     # mainPath = r'fits/jw02736-o001_t001_nircam_clear-f090w_i2d.fits' # All nan
     # mainPath = r'fits/jw01181-o009_t009_nircam_clear-f090w_i2d.fits' # All off image
     mainPath = r'fits/jw02514125001_03201_00002_nrca2_i2d.fits' # Some on image
-    targetPath = r'files/targets.csv'
+    targetPath = r'targets.csv'
 
     fitting_params = {'r_step': 0.1, 'r_limit': 30, 'r_step_limit': 30}
     run(mainPath, targetPath, 'std', **fitting_params)
+
+    print('|---------------------------|\n Run-time: ', round(systime.time() - start, 4), 'seconds\n|---------------------------|')
